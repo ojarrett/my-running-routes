e6686dc00fd0bff238789a9d28c53e456f01ebae
Owen Jarrett <owen.t.jarrett@gmail.com>
2020-04-25 16:54:10 -0700

    Add test-related dependencies, create new run indicator drawable resources

--- client/app/src/androidTest/java/com/ojarrett/myrunningroutes/LandingPageTest.kt
+++ client/app/src/androidTest/java/com/ojarrett/myrunningroutes/LandingPageTest.kt
@@ -1,18 +1,29 @@
 package com.ojarrett.myrunningroutes
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withResourceName
+import androidx.test.rule.ActivityTestRule
 import org.junit.Before
 import org.junit.Test
 
 import androidx.test.runner.AndroidJUnit4
+import org.junit.Rule
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class LandingPageTest {
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
     fun clickRunSelectorBasic() {
         // User loads app and should see 3 grey circles indicating the run to select
+        onView(withId(R.id.circle_run_1))
+            .perform(click())
+            .check(matches(withResourceName("run_selector.xml")))
 
         // User clicks one of the circles and observes that it turns white
 
@@ -23,6 +34,9 @@ class LandingPageTest {
     @Test
     fun clickRunSelectorActions() {
         // User loads app and clicks one of the grey circles to record a new run
+        onView(withId(R.id.circle_run_1))
+            .perform(click())
+            .check(matches(withResourceName("run_selector.xml")))
 
         // User clicks the "Start" button to start recording their run and observes that
         // the selected circle turns green
